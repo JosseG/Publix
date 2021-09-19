e4fea6595b4cff187775ca828b62f93cdb5b5fec
JosseG <jq994435258@gmail.com>
2021-09-19 13:36:58 -0500

    SextoCambio

--- src/proyectoSemana3/Frm2.java
+++ src/proyectoSemana3/Frm2.java
@@ -59,7 +59,7 @@ public class Frm2 extends JFrame {
 		lblNewLabel_2.setBounds(32, 185, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("Holaa");
+		JButton btnNewButton = new JButton("Holaaa");
 		btnNewButton.setForeground(Color.BLUE);
 		btnNewButton.setBackground(Color.BLUE);
 		btnNewButton.setBounds(29, 236, 558, 14);
